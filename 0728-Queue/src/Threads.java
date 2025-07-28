0025d3cdace010208615bb5e480eac10f3889952
JosephTao_FCU <josttw@gmail.com>
2025-07-28 15:31:26 +0800

    Add Threads class demonstrating thread creation using inheritance, Runnable interface, and lambda expressions

---
+++ 0728-Queue/src/Threads.java
@@ -0,0 +1,29 @@
+public class Threads {
+    // 方法一：繼承Thread類別
+    static class MyThread extends Thread {
+        public void run() {
+            System.out.println("方法一：繼承Thread類別");
+        }
+    }
+
+    // 方法二：實作Runnable介面
+    static class MyRunnable implements Runnable {
+        public void run() {
+            System.out.println("方法二：實作Runnable介面");
+        }
+    }
+
+    public static void main(String[] args) {
+        // 方法一
+        Thread t1 = new MyThread();
+        t1.start();
+
+        // 方法二
+        Thread t2 = new Thread(new MyRunnable());
+        t2.start();
+
+        // 方法三：使用Lambda表達式
+        Thread t3 = new Thread(() -> System.out.println("方法三：使用Lambda表達式"));
+        t3.start();
+    }
+}
